2c7fe96704177ae0ae56a319d8f01ede5df31dd4
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-05 14:44:03 +0100

    Function default parameter

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,6 @@
+/*
+ * Run "npm run dev maintest/3-main.js" to test
+*/
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
