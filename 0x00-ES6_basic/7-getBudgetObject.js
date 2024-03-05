b1a27e9ad84142592b98294148591225f78dbbb5
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-05 19:34:01 +0100

    Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,7 @@
+/*
+ * Run "npm run dev maintest/7-main.js" to test
+*/
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = { income, gdp, capita };
+  return budget;
+}
