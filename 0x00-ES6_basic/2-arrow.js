a432bea3d9818f5fdd80d62e954e38cb729d9da7
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-05 14:15:09 +0100

    To arrow function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+/*
+ * Run "npm run dev maintest/2-main.js" to test
+*/
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
