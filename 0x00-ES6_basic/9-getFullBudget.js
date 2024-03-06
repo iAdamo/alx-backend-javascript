75cb93c1680d4a5113b91f13cf7c3f2b50e58241
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-06 16:19:28 +0100

    ES6 method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,19 @@
+/*
+ * Run "npm run dev maintest/9-main.js" to test
+*/
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
