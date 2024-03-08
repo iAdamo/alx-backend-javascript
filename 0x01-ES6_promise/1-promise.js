acaa6f8a42e47288715ab842e79bd562a622248f
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-08 19:34:48 +0100

    Promise with errors

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+/*
+ * Run "npm run dev maintest/1-main.js" to test
+*/
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
