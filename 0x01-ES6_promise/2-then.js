c1c6f4d263bfd862f04eb3d8852e5910bfc5fb44
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-08 22:53:13 +0100

    Promise handlers

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+/*
+ * Run "npm run dev maintest/2-main.js" to test
+*/
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
