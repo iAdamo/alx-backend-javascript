360d55a17120722c1ef13fa0b8de2d1b2a8e4af7
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-13 12:04:45 +0100

    Inheritance and override

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof floors !== 'number') throw new TypeError('floors must be a number');
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    if (typeof floors !== 'number') throw new TypeError('floors must be a number');
+    this._floors = floors;
+  }
+
+  /**
+   * returns an evacuation warning message on a spcific floor
+   */
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
