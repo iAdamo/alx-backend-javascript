0b43dfd798979dd32b02d2f24988f96b1fa1b83e
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-13 13:31:21 +0100

    Airport string code

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,17 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 export default class Airport {
   constructor(name, code) {
-    if (typeof name || typeof code !== 'string') {
-      throw new TypeError('name and code must be string');
-    }
     this._name = name;
     this._code = code;
   }
 
-  /**
-   * returns a string description of the airport code
-   */
-  toString() {
-    return `[object ${this._code}]`;
+  get [Symbol.toStringTag]() {
+    return this._code;
   }
 }
