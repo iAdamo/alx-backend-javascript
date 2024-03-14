bf0e8a7b59de62560dd0ff43547d59fa66370876
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-14 10:05:45 +0100

    Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('location must be a string');
+    }
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](value) {
+    if (value === 'number') return this._size;
+    if (value === 'string') return this._location;
+    return null;
+  }
+}
