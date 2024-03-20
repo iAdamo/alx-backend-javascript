1539ea586bff7337f8417699b2e676ce663e3a39
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-20 12:06:24 +0100

    Clean set - startstring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,8 +6,15 @@
  */
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') return '';
-  return Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.substring(startString.length))
-    .join('-');
+  const parts = [];
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
