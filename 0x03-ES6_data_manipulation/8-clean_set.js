4820b2c977ca1f9d06b9b9ae153f22534923a90d
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-20 11:41:16 +0100

    Clean set - startstring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,11 @@
+/**
+ * returns a string of all the set values that start with a specific string (startString).
+ * @param {*Set} set of array values
+ * @param {*String} startString to filter the set values
+ * @returns a string string contains all the values of the set separated by -.
+ */
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (!startString || typeof startString !== 'string') return '';
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length))
